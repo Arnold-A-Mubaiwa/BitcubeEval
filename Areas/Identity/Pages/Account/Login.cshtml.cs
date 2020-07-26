7be18eb77f357e91bd10b25f9d0f74626b796453
Arnold <arnold@localhost.localdomain>
2020-07-26 15:42:07 +0200

    Section 2

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -73,7 +73,7 @@ namespace BitcubeEval.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("/Identity/Account/Manage/Index");
 
             if (ModelState.IsValid)
             {
